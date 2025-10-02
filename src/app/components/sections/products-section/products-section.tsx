e22779287b0b1339b9050d780edcee07072d3c4d
caiohperlin <caiohperlin@gmail.com>
2025-10-01 22:05:54 -0400

    feat: start products section

--- src/app/components/sections/products-section/products-section.tsx
+++ src/app/components/sections/products-section/products-section.tsx
@@ -4,7 +4,7 @@ import style from "./products-section.module.css";
 export default function ProductsSection() {
 	return (
 		<SectionWrapper className={style.productsSectionWrapper}>
-			<section className={style.productsSection}>secao produtos</section>
+			<section className={style.productsSection}></section>
 		</SectionWrapper>
 	);
 }
