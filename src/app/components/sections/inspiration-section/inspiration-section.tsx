db1a9546583070525f7789aa16f857aa3594fefd
Shishura <Murilotoledo2015@gmail.com>
2025-10-01 22:01:03 -0400

    feat: creat-inspiration-page

---
+++ src/app/components/sections/inspiration-section/inspiration-section.tsx
@@ -0,0 +1,7 @@
+import { section } from "motion/react-client";
+import SectionWrapper from "../../section-wrapper/section-wrapper";
+import style from "./inspiration-section.module.css";
+
+export default function InspirationSection() {
+	return <SectionWrapper></SectionWrapper>;
+}
---
+++ src/app/inspiration/page.tsx
@@ -0,0 +1,9 @@
+import SectionWrapper from "../components/section-wrapper/section-wrapper";
+
+export default function InspirationPage() {
+	return (
+		<div>
+			<></>
+		</div>
+	);
+}
