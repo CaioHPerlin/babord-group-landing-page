ec4275f4224f4579957fff97f167c43cfa04fed8
caiohperlin <caiohperlin@gmail.com>
2025-11-17 17:36:27 -0400

    chore(products-section): populate products array

--- src/app/components/sections/products-section/products-section.tsx
+++ src/app/components/sections/products-section/products-section.tsx
@@ -10,12 +10,35 @@ import product4Image from "@/app/assets/images/products-section/product-4.jpg";
 
 const productCards: ProductCardProps[] = [
 	{
+		title: "White fish",
 		slug: "cod",
-		title: "White Fish",
 		image: product1Image,
 		alt: "Cod fish prepared on a grill with herbs and specialties",
 		description:
 			"White fish prepared as fine fillets, loins, in portions, tails and B&P. Can also be supplied whole. Available fresh, frozen or chilled in a variety of packaging options.",
+	},
+	{
+		title: "Pink fish",
+		slug: "salmon",
+		image: product2Image,
+		alt: "Salmon fish served with vegetables and lemon",
+		description:
+			"Pink fish prepared as fine fillets A-F trim, portions on required specification or blocks of B&P and bellies. Available fresh, frozen or chilled in a variety of packaging options.",
+	},
+	{
+		title: "Breaded fish",
+		slug: "breaded-fish",
+		image: product3Image,
+		alt: "Breaded fish sticks served with lemon and sauce",
+		description:
+			"A varied selection of breaded products including fish fingers, burgers, fish fillets and many more. Available frozen in a variety of packaging options.",
+	},
+	{
+		title: "Babord Specials",
+		slug: "trout",
+		image: product4Image,
+		alt: "Trout fish served with herbs",
+		description: "Seafood specialties from Babord",
 	},
 ];
 
