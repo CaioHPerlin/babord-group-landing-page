a8b0126f78af44de0794417c176eeeea99e32f8a
Shishura <murilotoledo2015@gmail.com>
2025-11-24 02:34:17 -0400

    chore: add image story-section

--- src/app/components/sections/landing-page/story-section/story-section.tsx
+++ src/app/components/sections/landing-page/story-section/story-section.tsx
@@ -1,5 +1,19 @@
 import SectionWrapper from "../../../section-wrapper/section-wrapper";
+import style from "./story-section.module.css";
 
 export function StorySection() {
-	return <SectionWrapper></SectionWrapper>;
+	return (
+		<SectionWrapper>
+			<div className={style.imageWrapper}>
+				{/* Usa a imagem em `public/background-story.jpg` conforme solicitado */}
+				<img src="/background-story.jpg" alt="Story background" />
+			</div>
+
+			<section className={style.storySection}>
+				{/* Conteúdo da seção (texto, botões, etc.) pode ser adicionado aqui por você */}
+			</section>
+		</SectionWrapper>
+	);
 }
+
+export default StorySection;
